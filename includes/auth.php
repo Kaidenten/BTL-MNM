8b0ebd30d96bf44690f3af85901f9f23337d719a
Kaidenten <154009944+Kaidenten@users.noreply.github.com>
2026-05-17 07:30:17 +0700

    Delete includes directory

--- includes/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-if (empty($_SESSION['user_id'])) {
-    header('Location: /BTL-MNM/pages/login.php?redirect=' . urlencode($_SERVER['REQUEST_URI']));
-    exit();
-}
